82d8f31c58d3a37c499dcd8c87a516279692c406
neason-cn <cqwjsl123@126.com>
2021-11-09 20:13:48 +0800

    init config spi

---
+++ jbaas-spi/src/main/java/com/jbaas/infrastructure/spi/config/ConfigAbility.java
@@ -0,0 +1,25 @@
+package com.jbaas.infrastructure.spi.config;
+
+import java.util.List;
+
+/**
+ * Config Ability Interface
+ *
+ * @author neason-cn
+ * @date 2021/11/9
+ */
+public interface ConfigAbility {
+
+    String get(String dataId, String group, long timeout);
+
+    Boolean publist(String dataId, String group, String content);
+
+    Boolean remove(String dataId, String group);
+
+    void addListener(String dataId, String group, ConfigListener listener);
+
+    void removeListener(String dataId, String group, ConfigListener listener);
+
+    List<ConfigListener> getListeners(String dataId, String group);
+
+}
---
+++ jbaas-spi/src/main/java/com/jbaas/infrastructure/spi/config/ConfigListener.java
@@ -0,0 +1,18 @@
+package com.jbaas.infrastructure.spi.config;
+
+/**
+ * Config Listener
+ *
+ * @author neason-cn
+ * @date 2021/11/9
+ */
+public interface ConfigListener {
+
+    /**
+     * execute when config changes
+     *
+     * @param config remote config value
+     */
+    void receive(final String config);
+
+}
